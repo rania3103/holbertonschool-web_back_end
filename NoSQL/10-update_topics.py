01f9336e4cbcff88d33f7640241fec149a670b8d
rania3103 <raniaabassi33@gmail.com>
2024-01-09 14:37:04 +0100

    fix it to update many

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,4 +5,5 @@ all topics of a school document based on the name"""
 
 def update_topics(mongo_collection, name, topics):
     """update"""
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many(
+        {"name": name}, {"$set": {"topics": topics}})
