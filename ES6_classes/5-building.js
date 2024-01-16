926022a9e514c390ff33fdcb6716442aacec95b2
rania3103 <raniaabassi33@gmail.com>
2024-01-16 09:50:47 +0100

    A Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') {
+      throw TypeError('constructor attribute must be number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('sqft must be number');
+    }
+    this._sqft = sqft;
+  }
+}
