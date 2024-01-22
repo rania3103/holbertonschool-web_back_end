ffa93352e08f8075447d81fdf905257d862e7670
rania3103 <raniaabassi33@gmail.com>
2024-01-22 17:02:23 +0100

    dealing with symbols in es6

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+export default class Car {
+  constructor(brand, motor, color) {
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this[_brand], this[_motor], this[_color]);
+  }
+}
