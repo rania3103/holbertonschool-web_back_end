e23c0f7f0b28a363817f8f98b434324b690c61db
rania3103 <raniaabassi33@gmail.com>
2024-01-26 11:19:34 +0100

    Catch me if you can!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,10 +2,11 @@ export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise
       .then(() => {
+        console.log('Got a response from the API');
         resolve({ status: 200, body: 'success' });
       }).catch(() => {
+        console.log('Got a response from the API');
         reject(new Error());
-      })
-      .finally(() => console.log('Got a response from the API'));
+      });
   });
 }
