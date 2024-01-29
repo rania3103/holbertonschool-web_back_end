21faab9af62870920a7ac92756d8ce7152e47919
rania3103 <raniaabassi33@gmail.com>
2024-01-29 16:34:53 +0100

    try to fix code

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   return [...set].reduce((acc, str) => {
     let setFiltered = acc;
-    if (str && str.startsWith(startString) && startString !== '') {
+    if (typeof startString === 'string' && str && str.startsWith(startString) && startString !== '') {
       setFiltered += `${str.slice(startString.length)}-`;
     }
     return setFiltered;
